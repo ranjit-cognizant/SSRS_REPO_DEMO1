71b5021a51db8f4aebda2f814218488b98f108fb
alocalbox <alocalbox@VM-SQLSVR-TMG-F>
2022-07-27 12:42:38 -0400

    ..............

--- SSRS_REPORT/DeploySSRSReportsTest1.ps1
+++ SSRS_REPORT/DeploySSRSReportsTest1.ps1
@@ -6,7 +6,7 @@ Param(
 #[string] $DatabaseName = "Facetsext",
 #[string] $TargetFolder = "MyReports",
 #[string] $DataSourceUserName = "alocalbox",
-#[string] $DataSourcePassword = "TriZett02022"
+##[string] $DataSourcePassword = "TriZett02022"
 
 [string] $DataDataSourceFolder,
 [string] $DataSourceFile = "DataSource1.rds", 
