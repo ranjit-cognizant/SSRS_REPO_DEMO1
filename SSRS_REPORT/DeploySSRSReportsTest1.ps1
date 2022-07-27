49a437787443bc6a9b791295fb50f0b01d161d4d
alocalbox <alocalbox@VM-SQLSVR-TMG-F>
2022-07-27 12:21:39 -0400

    commit changes

--- SSRS_REPORT/DeploySSRSReportsTest1.ps1
+++ SSRS_REPORT/DeploySSRSReportsTest1.ps1
@@ -11,7 +11,7 @@ Param(
 [string] $DataSourceFolder,
 [string] $DataSourceFile = "DataSource1.rds", 
 [string] $ReportServerUri = "http://vm-sqlsvr-tmg-f/ReportServer/ReportService2010.asmx?wsdl",
-[string] $DBServerName = "VM-SQLSVR-TMG-H", 
+[string] $DBServerName = "VM-SQLSVR-TMG-F", 
 [string] $DatabaseName = "Facetsext",
 [string] $TargetFolder = "MyReports",
 [string] $DataSourceUserName = "alocalbox",
